b7eceb03f77020d15c23b13f26eba2515b0c2a14
vyaivanove <vyaivanove@yandex-team.ru>
2024-11-02 23:55:21 +0300

    Ultidraw-1: Initialize project

---
+++ app/src/main/java/com/vyaivanove/ultidraw/editor/EditorActivity.kt
@@ -0,0 +1,26 @@
+package com.vyaivanove.ultidraw.editor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+
+class EditorActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Text(
+                    text = "Android",
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
+        }
+    }
+}
